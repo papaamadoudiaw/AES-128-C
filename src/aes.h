4a2bf24060fc3328593fc6c8b43f2b9a422f9e78
Papa amadou <papaamadoudiaw422@email.com>
2026-09-07 11:22:36 +0200

    Etape J: fonctions de dechiffrement AES-128

--- src/aes.h
+++ src/aes.h
@@ -9,4 +9,8 @@ void mix_columns(uint8_t *bloc);
 void add_round_key(uint8_t *bloc, const uint8_t *round_key);
 void key_expansion(const uint8_t *cle, uint8_t *round_keys);
 void aes128_encrypt(const uint8_t *bloc_entree, const uint8_t *cle, uint8_t *bloc_sortie);
+void inv_sub_bytes(uint8_t *bloc);
+void inv_shift_rows(uint8_t *bloc);
+void inv_mix_columns(uint8_t *bloc);
+void aes128_decrypt(const uint8_t *bloc_entree, const uint8_t *cle, uint8_t *bloc_sortie);
 #endif
